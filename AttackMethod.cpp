7758def3e9ab5ba40afc261ce07701b65e3b45fd
nash <nashdev255@gmail.com>
2023-12-06 09:40:40 +0900

    Update AttackMethod.cpp

--- AttackMethod.cpp
+++ AttackMethod.cpp
@@ -1,8 +1,8 @@
 #include "./AttackMethod.hpp"
 
-inline AttackMethod :: AttackMethod(std::string const name, ElementalAttributeList const element, int const power) : name(name), element(element), power(power) {}
+inline AttackMethod :: AttackMethod(std::string& const name, ElementalAttributeList const element, int const power) : name(name), element(element), power(power) {}
 
-void AttackMethod :: setParams(std::string const name, ElementalAttributeList const element, int const power) {
+void AttackMethod :: setParams(std::string& const name, ElementalAttributeList const element, int const power) {
     this->name = name;
     this->element = element;
     this->power = power;
